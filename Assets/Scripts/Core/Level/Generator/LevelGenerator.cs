977401c644b9e428d581887bfc812baf84cde828
Andrii <101969951+MechanikXD@users.noreply.github.com>
2025-07-20 22:32:45 +0300

    Add placing rooms in world, and relative to other exits.

---
+++ Assets/Scripts/Core/Level/Generator/LevelGenerator.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using Core.Level.Room;
+using UnityEngine;
+using Random = System.Random;
+
+namespace Core.Level.Generator {
+    public class LevelGenerator : MonoBehaviour {
+        private Transform _main;
+        [SerializeField] private Transform origin;
+        [Space]
+        [SerializeField] private RoomInfo startRoom;
+        [SerializeField] private RoomInfo[] rooms;
+        [SerializeField] private RoomInfo endRoom;
+
+        private List<Transform> _availableExits;
+        private Random _seedGenerator;
+        private Random _levelGenerator;
+        private int _seed;
+
+        private void Awake() {
+            Init();
+            PlaceStart();
+            PlaceRandomRoom(rooms[1], _availableExits[0]);
+            PlaceRandomRoom(rooms[0], _availableExits[1]);
+            PlaceRandomRoom(rooms[0], _availableExits[1]);
+            PlaceRandomRoom(rooms[0], _availableExits[1]);
+        }
+
+        public void Init() {
+            _seedGenerator = new Random();
+            _seed = _seedGenerator.Next();
+            _levelGenerator = new Random(_seed);
+            _availableExits = new List<Transform>();
+        }
+
+        public void PlaceStart() {
+            var level = new GameObject("Level") {
+                transform = {
+                    position = origin.position
+                }
+            };
+
+            _main = level.transform;
+            var start = startRoom.InstantiateOnZero(_main);
+            _availableExits.AddRange(start.ExitPositions);
+        }
+
+        public void PlaceRandomRoom(RoomInfo roomToPlace, Transform entrancePosition) {
+            var roomExits = roomToPlace.ExitPositions;
+            
+            foreach (var exit in roomExits) {
+                //if (!roomToPlace.CanBePlacedAt(entrancePosition, exit, _main)) continue;
+                var generatedRoom = roomToPlace.InstantiateSelf(entrancePosition, exit, _main);
+                _availableExits.Remove(entrancePosition);
+                _availableExits.AddRange(generatedRoom.ExitPositions);
+                break;
+            }
+        }
+
+        public void PlaceRandomRoom(Transform exitPosition) {
+            var randomRoom = rooms[_levelGenerator.Next(0, rooms.Length)];
+            PlaceRandomRoom(randomRoom, exitPosition);
+        }
+
+        public void PlaceRandomRoom() {
+            var randomExit = _availableExits[_levelGenerator.Next(0, _availableExits.Count)];
+            PlaceRandomRoom(randomExit);
+        }
+    }
+}
---
+++ Assets/Scripts/Core/Level/Room/RoomInfo.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Core.Level.Room {
+    public class RoomInfo : MonoBehaviour {
+        [SerializeField] private List<Transform> exits;
+        [SerializeField] private Transform[] enemySpawnPoints;
+        [SerializeField] private GameObject roomColliders;
+
+        public List<Transform> ExitPositions => exits;
+
+        public RoomInfo InstantiateSelf(Transform otherEntrance, Transform thisExit, Transform parent) {
+            // Get rotation room need to be facing
+            Quaternion targetRotation = otherEntrance.rotation * Quaternion.Euler(0, 180, 0);
+            // difference between two angles
+            Quaternion rotationDifference = targetRotation * Quaternion.Inverse(thisExit.rotation);
+            // Calculate where the room center should be placed (assuming room center is at 0,0,0)
+            Vector3 roomPosition = otherEntrance.position - rotationDifference * thisExit.localPosition;
+    
+            // Instantiate the room with the calculated position and rotation
+            var room = Instantiate(gameObject, roomPosition, rotationDifference);
+            room.transform.SetParent(parent, true);
+            var createdRoomInfo = room.GetComponent<RoomInfo>();
+    
+            // Remove the used exit
+            createdRoomInfo.exits.Remove(thisExit);
+            return createdRoomInfo;
+
+            // TODO: Check if other exits alligned/got blocked
+        }
+        
+        public RoomInfo InstantiateOnZero(Transform parent) {
+            return Instantiate(gameObject, parent, false).GetComponent<RoomInfo>();
+        }
+        
+        public bool CanBePlacedAt(Transform otherEntrance, Transform thisExit, Transform parent) {
+            /*var rotation = -thisExit.rotation.eulerAngles;
+            var position = thisExit.position;
+            if (rotation != Vector3.zero) {
+                position = RotatePointAroundPivot(position, Vector3.zero, rotation);
+            }
+            
+            var checkCollider = Instantiate(roomColliders, otherEntrance.position - position, Quaternion.Euler(rotation));
+            checkCollider.transform.SetParent(parent, false);
+            // TODO: wrong was of detecting collision
+            var hasContacts = roomColliders.GetComponents<BoxCollider>().Any(c => c.providesContacts);
+            Destroy(checkCollider);
+            return !hasContacts;*/
+            return true;
+        }
+        
+        public Vector3 RotatePointAroundPivot(Vector3 point, Vector3 pivot, Vector3 angles) {
+            return Quaternion.Euler(angles) * (point - pivot) + pivot;
+        }
+    }
+}
