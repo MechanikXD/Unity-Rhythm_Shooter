a1f2fbe92368aee8fd1b357a5e34413b0f1d8908
Andrii <101969951+MechanikXD@users.noreply.github.com>
2025-08-12 09:33:44 +0300

    Expand BaseBehaviourInjection.cs to remember possible behaviours.

--- Assets/Scripts/Core/Behaviour/BehaviourInjection/BaseBehaviourInjection.cs
+++ Assets/Scripts/Core/Behaviour/BehaviourInjection/BaseBehaviourInjection.cs
@@ -1,17 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Core.Behaviour.BehaviourInjection {
     public class BaseBehaviourInjection<TAccept, TReturn> {
         private Func<TAccept, TReturn> _currentBehavior;
         private readonly Func<TAccept, TReturn> _defaultBehavior;
 
+        private readonly Dictionary<string, Func<TAccept, TReturn>> _behaviours;
+
         public BaseBehaviourInjection(Func<TAccept, TReturn> defaultBehavior) {
+            _behaviours = new Dictionary<string, Func<TAccept, TReturn>>
+                { { "Default", defaultBehavior } };
             _currentBehavior = defaultBehavior;
             _defaultBehavior = defaultBehavior;
         }
         
         public BaseBehaviourInjection(Func<TAccept, TReturn> defaultBehavior, 
                                       Func<TAccept, TReturn> activeBehaviour) {
+            _behaviours = new Dictionary<string, Func<TAccept, TReturn>>
+                { { "Default", defaultBehavior } };
             _currentBehavior = activeBehaviour;
             _defaultBehavior = defaultBehavior;
         }
@@ -23,19 +30,27 @@ namespace Core.Behaviour.BehaviourInjection {
 
         public void ChangeBehaviour(Func<TAccept, TReturn> newBehaviour) => 
             _currentBehavior = newBehaviour;
+
+        public void AddBehaviour(string key, Func<TAccept, TReturn> behaviour) =>
+            _behaviours.Add(key, behaviour);
+        
+        public void ChangeBehaviour(string key) => ChangeBehaviour(_behaviours[key]);
     }
 
     public class BaseBehaviourInjection<T> {
         private Action<T> _currentBehavior;
         private readonly Action<T> _defaultBehavior;
         
+        private readonly Dictionary<string, Action<T>> _behaviours;
+        
         public BaseBehaviourInjection(Action<T> defaultBehavior) {
+            _behaviours = new Dictionary<string, Action<T>> { { "Default", defaultBehavior } };
             _currentBehavior = defaultBehavior;
             _defaultBehavior = defaultBehavior;
         }
         
-        public BaseBehaviourInjection(Action<T> defaultBehavior, 
-                                      Action<T> activeBehaviour) {
+        public BaseBehaviourInjection(Action<T> defaultBehavior, Action<T> activeBehaviour) {
+            _behaviours = new Dictionary<string, Action<T>> { { "Default", defaultBehavior } };
             _currentBehavior = activeBehaviour;
             _defaultBehavior = defaultBehavior;
         }
@@ -47,5 +62,9 @@ namespace Core.Behaviour.BehaviourInjection {
 
         public void ChangeBehaviour(Action<T> newBehaviour) => 
             _currentBehavior = newBehaviour;
+        
+        public void AddBehaviour(string key, Action<T> behaviour) => _behaviours.Add(key, behaviour);
+        
+        public void ChangeBehaviour(string key) => ChangeBehaviour(_behaviours[key]);
     }
 }
